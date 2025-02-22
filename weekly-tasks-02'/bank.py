68e09d0da5987b022992e5cfeb78474c490775a3
Linda Nikol <nlinda1105@outlook.com>
2025-02-22 14:48:05 +0000

    setting up SSH key for weekly tasks folder

---
+++ weekly-tasks-02'/bank.py
@@ -0,0 +1,7 @@
+# Programme that prompts the user 
+# And read in two money amounts (in cent)
+# Then adds the two amounts
+# Then prints out the answer in a human readable format 
+# With a euro sign and decimal point between the euro and cent of the amount 
+# Author Linda Nikolajeva
+# Part of coursework for ATU 24-25: 4122 -- PROGRAMMING AND SCRIPTING
